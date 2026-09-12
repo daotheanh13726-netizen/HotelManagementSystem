249058b28a028e7dd7240d8a1b943c5b095b4039
DAOTHEANH <theanhdao55@gmail.com>
2026-09-12 19:04:32 +0700

    connet csdl

---
+++ HotelManagementSystem.API/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using HotelManagementSystem.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelManagementSystem.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TestController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("database")]
+        public async Task<IActionResult> TestDatabase()
+        {
+            var count = await _context.RoomTypes.CountAsync();
+
+            return Ok(new
+            {
+                message = "Kết nối database thành công",
+                roomTypeCount = count
+            });
+        }
+    }
+}
---
+++ HotelManagementSystem.API/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using HotelManagementSystem.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelManagementSystem.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RoomType> RoomTypes { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RoomType>()
+                .ToTable("LoaiPhong");
+
+            modelBuilder.Entity<Room>()
+                .ToTable("Phong");
+        }
+    }
+}
