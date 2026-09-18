a713427022f9c91175f6c582881b171ce28134ce
DAOTHEANH <theanhdao55@gmail.com>
2026-09-18 10:00:47 +0700

    Hoàn thành CRUD API quản lý phòng

---
+++ HotelManagementSystem.API/Controllers/RoomController.cs
@@ -0,0 +1,92 @@
+﻿using HotelManagementSystem.API.Data;
+using HotelManagementSystem.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelManagementSystem.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RoomController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public RoomController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Room
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var rooms = await _context.Rooms.ToListAsync();
+
+            return Ok(rooms);
+        }
+
+        // GET: api/Room/1
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var room = await _context.Rooms.FindAsync(id);
+
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(room);
+        }
+
+        // POST: api/Room
+        [HttpPost]
+        public async Task<IActionResult> Create(Room room)
+        {
+            _context.Rooms.Add(room);
+            await _context.SaveChangesAsync();
+
+            return Ok(room);
+        }
+
+        // PUT: api/Room/1
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Room room)
+        {
+            var existingRoom = await _context.Rooms.FindAsync(id);
+
+            if (existingRoom == null)
+            {
+                return NotFound();
+            }
+
+            existingRoom.RoomNumber = room.RoomNumber;
+            existingRoom.Status = room.Status;
+            existingRoom.RoomTypeId = room.RoomTypeId;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(existingRoom);
+        }
+
+        // DELETE: api/Room/1
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var room = await _context.Rooms.FindAsync(id);
+
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            _context.Rooms.Remove(room);
+            await _context.SaveChangesAsync();
+
+            return Ok(new
+            {
+                message = "Xóa phòng thành công"
+            });
+        }
+    }
+}
